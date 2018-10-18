ac23e93fbf934efd7e369df4cbc0a4c6d5458958
yangms <yangms@geo-compass.com>
2018-10-18 08:57:01 +0800

    测试一下

--- app/src/main/java/com/yangms/music/MainActivity.java
+++ app/src/main/java/com/yangms/music/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTvShow = findViewById(R.id.tv_show);
-
+        mTvShow.setText("music");
 
     }
 
